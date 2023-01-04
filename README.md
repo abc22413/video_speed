49e3a6fb0f4fdcecefe85db8d2c02e54054600cf
abc22413 <45685901+abc22413@users.noreply.github.com>
2023-01-05 02:42:45 +0800

    full UI skeleton

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# video speed
- Video speed controller plugin for Chrome
+# Video Speed Controller
+Video speed controller plugin for Chrome. Optimized for HTML5 videos and Panopto.
 
-HTML5 video
-Panopto video library
+## Installation
+1. Download the folder
+2. Go to chrome://extensions
+3. Enable developer mode
+4. Click "Load Unpacked" and select the folder
+
+## Usage
+- Select speed via text input
+- Select speed via presets
+- Select speed via slider
 
 ## Todos
 - v1
 - Use popup instead of alerts
+- Allow color personalization and persistence
+
+## v2
+- Allow enable/disable
+- Allow blocklists
+- Allow personalization of speed presets
+- Allow shortcuts & customized keybinding
+- Allow personalization of slider notches
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,28 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var speed = 1.0;
+    //Global variables
+    var speed = 1.00;
+    var current_speed = document.getElementById('current-speed');
+    //Text input
     var speed_select_button = document.getElementById('speed-select-submit');
     var speed_select_input = document.getElementById('speed-select-input');
-    var current_speed = document.getElementById('current-speed');
+    //Presets
+    var preset_speeds = [0.50, 1.00, 1.50, 2.00, 3.00, 5.00, 7.00, 16.00];
 
+    //Generate button presets
+    innerhtml_inject = "";
+    for (let i=0; i<4; i++) {
+
+    }
+
+    //Utility function: Update all displays on speed change
+    function update_displays() {
+        current_speed.innerHTML = `<p>Current Speed: <b>${speed}x</b></p>`;
+    }
+
+    //Enter on text input triggers text input submit
+    speed_select_input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          speed_select_button.click();
+        }
+    })
+
+    //Text Input
     speed_select_button.addEventListener('click', function() {
         //Read, clean and convert speed text input
         text_input = speed_select_input.value;
+        speed_select_input.value = '';
         extracted_speed = parseFloat(text_input).toFixed(2);
+        //If blank, count as mispress
+        if (text_input===''){}
+        else{
         //Invalid input
         if (isNaN(extracted_speed)===true) {
             alert('Please input a decimal between 0-16');
-            speed_select_input.value = '1.00';
         }
         //Input out of range
         else if (extracted_speed<0.00 || extracted_speed>16.00) {
             alert('Please input a decimal between 0-16');
-            speed_select_input.value = '1.00';
         }
         //Proper input
         else {
+            //Internal housekeeping
             speed = extracted_speed;
-            current_speed.innerHTML = `<p>Current Speed: <b>${speed}</b></p>`;
-        }
-    }, false);
+            update_displays();
+            //Post message to alter video speed: popup->background->content
 
-}, false);
+        }
+        }
+    }, false)
+
+}, false)
