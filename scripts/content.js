ae468f19260f74572f15ba5fdc6349305aadca9b
abc22413 <45685901+abc22413@users.noreply.github.com>
2023-01-07 15:12:35 +0800

    cleanup content.js

--- scripts/content.js
+++ scripts/content.js
@@ -4,7 +4,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.purpose === "coelenterazine_change_video_speed") {
             try {
                 new_speed = request.new_speed;
-                //Panopto
+
+                //Panopto window
                 if (document.getElementById('panoptoLogoTemplate') !== null) {
                     document.getElementById('playButton').click();
                     document.getElementsByTagName('video')[0].playbackRate = new_speed;
@@ -12,10 +13,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                     document.getElementById('playButton').click();
                     sendResponse({"success": true});
                 }
+
                 //HTML5 video
-                else if (document.querySelector('video') !== null) {
+                else if (document.querySelector('video') !== null || document.getElementById('instructure_ajax_error_box') !== null) {
                     document.querySelector('video').playbackRate = new_speed;
                     sendResponse({"success": true});
+                    return;
                 }
             }
             catch (err) {
